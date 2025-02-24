6132ee59cfc91df87ad990623208046626467804
Luiz Fernando <fernandinho.manon@gmail.com>
2025-02-23 22:40:59 -0300

    feat: Adicionar arquivo variaveis.dart

---
+++ aula-01-introducao-a-variaveis/variaveis.dart
@@ -0,0 +1,19 @@
+// Aula 01 - Introdução a variáveis
+
+void main() {
+  // Declara e imprime uma variável do tipo String
+  String variavelNome = 'Luiz';
+  print(variavelNome);
+
+  // Declara e imprime uma variável do tipo inteiro
+  int valorVariavel = 10;
+  print(valorVariavel);
+
+  // Declara e imprime uma variável booleana
+  bool ehVerdadeiro = true;
+  print(ehVerdadeiro);
+
+  // Declara e imprime uma lista de Strings
+  List<String> listaDePalavras = ['Luiz', 'Fernando'];
+  print('${listaDePalavras[0]} - ${listaDePalavras[1]}');
+}
